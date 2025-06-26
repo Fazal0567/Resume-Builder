1fbdf62d03e92d9ad3b6291c2947709ff38b6779
Fazal0567 <fazalalicontribute@gmail.com>
2025-06-27 04:58:13 +0530

    project complete

---
+++ frontend/src/components/custom/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer className="border-t mt-auto py-6">
+            <div className="container mx-auto px-4 text-center text-sm">
+               <a 
+                 href="https://mohdfazalali.netlify.app/" 
+                 target="_blank" 
+                 rel="noopener noreferrer"
+                 className="text-blue-500 hover:underline"
+               >
+                 Mohd Fazal Ali
+               </a> © {new Date().getFullYear()} ALL Rights Reserved
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
