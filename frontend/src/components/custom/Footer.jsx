a28f82522bbe1396e95ef4648eb6ceae39c7808e
Fazal0567 <fazalalicontribute@gmail.com>
2025-06-27 17:30:24 +0530

    change footer

--- frontend/src/components/custom/Footer.jsx
+++ frontend/src/components/custom/Footer.jsx
@@ -1,10 +1,15 @@
-
-
 const Footer = () => {
     return (
         <footer className="border-t mt-auto py-6">
             <div className="container mx-auto px-4 text-center text-sm">
-                © {new Date().getFullYear()} ResumeBuilder. Made by Ankit Jha.
+               <a 
+                 href="https://mohdfazalali.netlify.app/" 
+                 target="_blank" 
+                 rel="noopener noreferrer"
+                 className="text-blue-500 hover:underline"
+               >
+                 Mohd Fazal Ali
+               </a> © {new Date().getFullYear()} ALL Rights Reserved
             </div>
         </footer>
     )
