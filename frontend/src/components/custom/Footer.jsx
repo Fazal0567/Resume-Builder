54fe392c789313c491602ba790b16463524f38b8
Mohd Fazal Ali <fazalalicontribute@gmail.com>
2025-06-29 06:33:08 +0530

    Update Footer.jsx

--- frontend/src/components/custom/Footer.jsx
+++ frontend/src/components/custom/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="border-t mt-auto py-6">
             <div className="container mx-auto px-4 text-center text-sm">
                <a 
-                 href="https://mohdfazalali.netlify.app/" 
+                 href="https://mohd-fazal-ali.onrender.com/" 
                  target="_blank" 
                  rel="noopener noreferrer"
                  className="text-blue-500 hover:underline"
